60156aacfa4d59f7a6f3f922867f3463629c4a9e
Mark Kimsal <metrofindings@gmail.com>
2015-01-25 13:44:32 -0500

    Adjust time to allow for more election time.
    Let more time play out for leader election.

--- test.sh
+++ test.sh
@@ -8,9 +8,9 @@ php ./src/raft/node.php S1 tcp://127.0.0.1:5581 tcp://127.0.0.1:5580 tcp://127.0
 php ./src/raft/node.php S2 tcp://127.0.0.1:5582 tcp://127.0.0.1:5580 tcp://127.0.0.1:5581 tcp://127.0.0.1:5583 & node2=$!
 php ./src/raft/node.php S3 tcp://127.0.0.1:5583 tcp://127.0.0.1:5580 tcp://127.0.0.1:5581 tcp://127.0.0.1:5582 & node3=$!
 
-sleep 8;
+sleep 4;
 kill $node0;
-sleep 8;
+sleep 12;
 kill $node1;
 kill $node2;
 kill $node3;
