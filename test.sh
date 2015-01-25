90c4c4a41e0f14b02ed635ace4ce0ecab9dc8941
Mark Kimsal <metrofindings@gmail.com>
2015-01-25 13:42:41 -0500

    Micro sleep to help S0 be master.
    Fractional sleep only works on linux.

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 #php ./src/raft/node.php S1 ipc:///tmp/node1.ipc ipc:///tmp/node0.ipc & node1=$!
 
 php ./src/raft/node.php S0 tcp://127.0.0.1:5580 tcp://127.0.0.1:5581 tcp://127.0.0.1:5582 tcp://127.0.0.1:5583 & node0=$!
+sleep 0.1
 php ./src/raft/node.php S1 tcp://127.0.0.1:5581 tcp://127.0.0.1:5580 tcp://127.0.0.1:5582 tcp://127.0.0.1:5583 & node1=$!
 php ./src/raft/node.php S2 tcp://127.0.0.1:5582 tcp://127.0.0.1:5580 tcp://127.0.0.1:5581 tcp://127.0.0.1:5583 & node2=$!
 php ./src/raft/node.php S3 tcp://127.0.0.1:5583 tcp://127.0.0.1:5580 tcp://127.0.0.1:5581 tcp://127.0.0.1:5582 & node3=$!
