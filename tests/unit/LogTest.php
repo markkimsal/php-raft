9cce21bce23b241e8474e97988987ee3dcb477ee
Mark Kimsal <metrofindings@gmail.com>
2015-01-25 15:08:01 -0500

    Fix order of params

--- tests/unit/LogTest.php
+++ tests/unit/LogTest.php
@@ -9,7 +9,7 @@ class Raft_Log_Test extends PHPUnit_Framework_TestCase {
 
 	public function test_log_append_entry_is_pending() {
 		$this->assertEquals(0, $this->log->getCommitIndex());
-		$this->log->appendEntry('2', 'add');
+		$this->log->appendEntry('add', '2');
 		
 		$this->assertEquals(0, $this->log->getCommitIndex());
 		$this->assertEquals('2', $this->log->getPendingTerm());
@@ -18,7 +18,7 @@ class Raft_Log_Test extends PHPUnit_Framework_TestCase {
 
 	public function test_commit_increases_commit_index() {
 		$this->assertEquals(0, $this->log->getCommitIndex());
-		$this->log->appendEntry('2', 'add');
+		$this->log->appendEntry('add', '2');
 		$this->log->commitEntry();
 		
 		$this->assertEquals(1, $this->log->getCommitIndex());
