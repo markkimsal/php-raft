da0a7f504b946d1e2f9e7286282e6fa515061029
Mark Kimsal <metrofindings@gmail.com>
2015-03-14 16:04:30 -0400

    Cleanup procs on ctrl-c

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 #php ./bin/startnode.php S0 ipc:///tmp/node0.ipc ipc:///tmp/node1.ipc & node0=$!
 #php ./bin/startnode.php S1 ipc:///tmp/node1.ipc ipc:///tmp/node0.ipc & node1=$!
 
@@ -7,6 +7,14 @@ sleep 0.1
 php ./bin/startnode.php S1 tcp://127.0.0.1:5581 tcp://127.0.0.1:5580 tcp://127.0.0.1:5582 tcp://127.0.0.1:5583 & node1=$!
 php ./bin/startnode.php S2 tcp://127.0.0.1:5582 tcp://127.0.0.1:5580 tcp://127.0.0.1:5581 tcp://127.0.0.1:5583 & node2=$!
 php ./bin/startnode.php S3 tcp://127.0.0.1:5583 tcp://127.0.0.1:5580 tcp://127.0.0.1:5581 tcp://127.0.0.1:5582 & node3=$!
+
+function cleanup {
+kill $node0;kill $node1;kill $node2;kill $node3;
+exit;
+}
+
+trap cleanup SIGINT SIGTERM EXIT
+
 
 sleep 4;
 kill $node0;
